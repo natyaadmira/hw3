690ead28601288f1d614a713da9d099381efb417
natyaadmira <35670024+natyaadmira@users.noreply.github.com>
2018-02-05 21:08:23 -0800

    Create mouse-cone.js

---
+++ mouse-cone.js
@@ -0,0 +1,11 @@
+function draw() {
+  var startX = width/2;
+  var startY = 10;
+  var endX = mouseX;
+  var endY = mouseY;
+  line(startX, startY, endX, endY);
+}
+
+function mousePressed(){
+	clear()
+}
